b1ae6a60fead7d4927bf17e226acbd3469d7424a
MG <mg84@bk.ru>
2018-10-07 18:58:01 +0300

    hw 1 task 1 'intro' cmpl

---
+++ intro/about.php
@@ -0,0 +1,19 @@
+<?php
+$name = 'Batman';
+$age = 33;
+$email = 'gg@hrbr.mc';
+$city = 'Gotem';
+$bio = 'Full-stack developer'
+?>
+
+<h3>Страница пользователя <?php echo $name ?></h3>
+<b>Имя</b>
+<dd><?php echo $name ?></dd>
+<b>Возраст</b>
+<dd><?php echo $age ?></dd>
+<b>Адрес электронной почты</b>
+<dd><?php echo $email ?></dd>
+<b>Город</b>
+<dd><?php echo $city ?></dd>
+<b>О себе</b>
+<dd><?php echo $bio ?></dd>
