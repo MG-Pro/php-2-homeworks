04361818293a3544fd036523f7c407fe2fd0041c
MG <mg84@bk.ru>
2018-10-09 19:39:32 +0300

    hw 1 additional task 'fibo' fix

--- intro/task2.php
+++ intro/task2.php
@@ -1,26 +1,35 @@
 <?php
-
-$x = rand(0,100);
-echo $x;
-echo PHP_EOL;
+$x = rand(0, 100);
 
 $a = 1;
 $b = 1;
 
-while (true) {
-  if($a > $x) {
-    echo PHP_EOL . 'Число ' . $x . ' НЕ входит в числовой ряд';
-    break;
-  } elseif ($a === $x) {
-    echo PHP_EOL . 'Число ' . $x . ' входит в числовой ряд';
-    break;
-  } else {
-    $c = $a;
-    $a += $b;
-    $b = $c;
-    echo $c . ' ';
-  }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<?php
+while ($a < $x) {
+  $c = $a;
+  $a += $b;
+  $b = $c;
+  echo $c . ' ';
+}
+if ($a > $x) {
+  echo PHP_EOL . 'Число ' . $x . ' НЕ входит в числовой ряд' . PHP_EOL;
+} elseif ($a === $x) {
+  echo PHP_EOL . 'Число ' . $x . ' входит в числовой ряд' . PHP_EOL;
 }
 
+?>
+</body>
+</html>
 
 
