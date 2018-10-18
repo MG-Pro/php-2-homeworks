33b4a5475fab37e46a69cc9da7830c36c353e043
MG <mg84@bk.ru>
2018-10-18 22:02:11 +0300

    hw 3 task 1 'visa' cmpl

--- hw3/task2_visa/visa.php
+++ hw3/task2_visa/visa.php
@@ -2,13 +2,45 @@
 define("URL", 'https://data.gov.ru/opendata/7704206201-country/data-20180609T0649-structure-20180609T0649.csv?encoding=UTF-8');
 define('RESERVE_URL', 'https://raw.githubusercontent.com/netology-code/php-2-homeworks/master/files/countries/opendata.csv');
 
+if(count($argv) < 2) {
+  exit("Введите название страны");
+}
+
 $data = file_get_contents(URL);
 
 if($data === false) {
   $data = file_get_contents(RESERVE_URL);
 }
+if($data === false) {
+  exit("Данные недоступны");
+}
 
-var_dump($data)
+$data = explode(PHP_EOL, $data);
+array_shift($data);
+
+
+$list = [];
+
+foreach ($data as $item) {
+  $country = str_getcsv($item);
+  array_shift($country);
+  array_splice($country, 1, 2);
+  $list[] = $country;
+}
+
+$targetCountry = false;
+
+foreach ($list as $country) {
+  if($country[0] === $argv[1]) {
+    $targetCountry = $country;
+    break;
+  }
+}
+if ($targetCountry === false) {
+  echo "Страна $argv[1] не найдена";
+} else {
+  echo "$country[0]: $country[1]";
+}
 
 ?>
 
